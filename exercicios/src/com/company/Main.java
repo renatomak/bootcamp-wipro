82e1fd7918a8af5df06ff01e507a7e12e24df783
renatomak <renato.mark.silva@gmail.com>
2022-04-02 12:16:45 -0300

    logica: exercicio 1 - acertar numero

---
+++ exercicios/src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.Locale;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in).useLocale(Locale.US);
+        Random random = new Random();
+
+        int RandomValue = random.nextInt(10);
+        System.out.println(RandomValue);
+
+        boolean isFind = false;
+        int total = 0;
+
+
+        do {
+            System.out.print("Faça sua escolha: (0 a 10): ");
+            int choice = scan.nextInt();
+            if(RandomValue == choice) {
+                isFind = true;
+            }
+            total += 1;
+
+        }while(!isFind);
+        System.out.println("Você acertou!\nNúmero de tentativas: " + total);
+        System.out.println("FIM!!!");
+
+    }
+}
